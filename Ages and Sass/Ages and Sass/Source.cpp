9fc0ab9e9c4b6264e64c96f20b2c79fffabf5a83
ShawnDH <37811310+ShawnDH@users.noreply.github.com>
2018-04-11 12:37:33 -0500

    update Ages and Sass

--- Ages and Sass/Ages and Sass/Source.cpp
+++ Ages and Sass/Ages and Sass/Source.cpp
@@ -12,16 +12,34 @@ int main()
 	{
 		std::cout << "Hey! You can't be " << userAge << " years old! \n Don't try to pull the wool over my eyes you unfrosted poptart." << std::endl;
 	}
-	else if (userAge == 1 | 2 | 3 | 4 | 5)
+	else if (userAge == 1 | userAge <= 5)
 	{
 		std::cout << "What? Did mom leave the computer open again?" << std::endl;
 	}
-	else if (userAge == 6 | 7 | 8 | 9 | 10 | 11 | 12)
+	else if (userAge == 6 | userAge <= 12)
 	{
+		std::cout << "Sorry, this sight is 13 and up kiddo. Why don't you go play some minecraft." << std::endl;
 	}
-
-
-
+	else if (userAge == 13 | userAge <= 18)
+	{
+		std::cout << "Get a job ya hippie." << std::endl;
+	}
+	else if (userAge == 19 | userAge <= 25)
+	{
+		std::cout << "Lol millennials." << std::endl;
+	}
+	else if (userAge == 26 | userAge <= 32)
+	{
+		std::cout << "You know? I'm gonna say it.. I don't care about you or your problems.." << std::endl;
+	}
+	else if (userAge == 33 | userAge <= 40)
+	{
+		std::cout << "I can't belive you life is nearly half over and you're spending it reading this." << std::endl;
+	}
+	else if (userAge > 40)
+	{
+		std::cout << "Can you even read this?" << std::endl;
+	}
 
 	system("pause");
 	return 0;
