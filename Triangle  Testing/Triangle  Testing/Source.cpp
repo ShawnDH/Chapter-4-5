c63effa095c5ffba9ace3dc79cb6831df3483838
ShawnDH <37811310+ShawnDH@users.noreply.github.com>
2018-04-19 10:09:28 -0500

    update Triangle Testing

--- Triangle  Testing/Triangle  Testing/Source.cpp
+++ Triangle  Testing/Triangle  Testing/Source.cpp
@@ -37,7 +37,7 @@ int main()
 			std::cin >> finish;
 		}
 		//Equilateral Triangle
-		else if (userInputTriangleType == "Equilateral")
+		else if (userInputTriangleType == "Equilateral" || userInputTriangleType == "equilateral")
 		{
 			equilateralTriangle();
 			std::cout << "Would you like to calculate another Triangle? \n Yes  or  No \n >";
@@ -108,7 +108,7 @@ void rightTriangle()
 		rightTriangle();
 	}
 } 
-//isosceles Triangle
+//isosceles Triangle   Finished
 void isoscelesTriangle()
 {
 	double sideA, sideB, sideC;
@@ -116,7 +116,7 @@ void isoscelesTriangle()
 	double perimeter;
 	std::string userInputSide;
 
-	std::cout << "Which side of the isosceles triangle do you want to solve? \n A(Height), B(Base), or C(Slope) \n >";
+	std::cout << "Which side of the isosceles triangle do you want to solve? \n A(Height), B(Base), or C(Altitude) \n >";
 	std::cin >> userInputSide;
 
 	if (userInputSide == "C" || userInputSide == "c")
@@ -127,7 +127,7 @@ void isoscelesTriangle()
 		std::cout << "What is the distance for side B. \n >";
 		std::cin >> sideB;
 
-		sideC = 0.5 * sqrt(pow(sideA, 2) + (4 * pow(sideB, 2)));
+		sideC = sqrt(((sideB/2) * (sideB/2)) + (sideA * sideA));
 		area = (sideA * sideB) / 2;
 		perimeter = 2 * sideA + sideB;
 		std::cout << "The distance for side C is " << sideC << "\nThe area of the triangle is " << area << "\nThe perimeter of the triangle is " << perimeter << std::endl;
@@ -139,8 +139,8 @@ void isoscelesTriangle()
 
 		std::cout << "What is the distance for side C. \n >";
 		std::cin >> sideC;
-		//(sqrt((sideA * sideA) + (sideC * sideC)))/2
-		sideB = 1;
+
+		sideB = (sqrt((sideC * sideC) - (sideA * sideA)))*2;
 		area = (sideA * sideB) / 2;
 		perimeter = 2 * sideA + sideB;
 		std::cout << "The distance for side B is " << sideB << "\nThe area of the triangle is " << area << "\nThe perimeter of the triangle is " << perimeter << std::endl;
@@ -153,7 +153,7 @@ void isoscelesTriangle()
 		std::cout << "What is the distance for side C. \n >";
 		std::cin >> sideC;
 
-		sideA = sqrt((sideB * sideB) + (sideC * sideC));
+		sideA = sqrt(((sideB / 2) * (sideB / 2)) - (sideC * sideC));
 		area = (sideA * sideB) / 2;
 		perimeter = 2 * sideA + sideB;
 		std::cout << "The distance for side A is " << sideA << "\nThe area of the triangle is " << area << "\nThe perimeter of the triangle is " << perimeter << std::endl;
@@ -174,42 +174,36 @@ void equilateralTriangle()
 
 	std::cout << "Which side of the Equilateral triangle do you want to solve? \n A(Height), B(Base), or C(Slope) \n >";
 	std::cin >> userInputSide;
-	if (userInputSide == "C" || "c")
+	if (userInputSide == "C" || userInputSide == "c")
 	{
-		std::cout << "You chose side " << userInputSide << ", what is the distance for side A. \n >";
+		std::cout << "You chose side " << userInputSide << ", what is the distance for side A or B. \n >";
 		std::cin >> sideA;
 
-		std::cout << "What is the distance for side B. \n >";
-		std::cin >> sideB;
-
-		sideC = sqrt((sideA * sideA) + (sideB * sideB));
-		area = (sideA * sideB) / 2;
+		sideC = sideA;
+		sideB = sideA;
+		area = (sqrt(3) / 4) * (sideA * sideA);
 		perimeter = sideA + sideB + sideC;
-		std::cout << "The distance for side C is " << sideC << "\nThe area of the triangle is " << area << "\nThe perimeter of the triangle is " << perimeter << std::endl;
+		std::cout << "The distance for side C, B, and A is " << sideC << "\nThe area of the triangle is " << area << "\nThe perimeter of the triangle is " << perimeter << std::endl;
 	}
-	if (userInputSide == "B" || "b")
+	if (userInputSide == "B" || userInputSide == "b")
 	{
-		std::cout << "You chose side " << userInputSide << ", what is the distance for side A. \n >";
+		std::cout << "You chose side " << userInputSide << ", what is the distance for side A or C. \n >";
 		std::cin >> sideA;
 
-		std::cout << "What is the distance for side C. \n >";
-		std::cin >> sideC;
-
-		sideB = sqrt((sideA * sideA) + (sideC * sideC));
-		area = (sideA * sideB) / 2;
+		sideC = sideA;
+		sideB = sideA;
+		area = (sqrt(3) / 4) * (sideA * sideA);
 		perimeter = sideA + sideB + sideC;
-		std::cout << "The distance for side B is " << sideB << "\nThe area of the triangle is " << area << "\nThe perimeter of the triangle is " << perimeter << std::endl;
+		std::cout << "The distance for side B, C, and A is " << sideB << "\nThe area of the triangle is " << area << "\nThe perimeter of the triangle is " << perimeter << std::endl;
 	}
-	if (userInputSide == "A" || "a")
+	if (userInputSide == "A" || userInputSide == "a")
 	{
-		std::cout << "You chose side " << userInputSide << ", what is the distance for side B. \n >";
+		std::cout << "You chose side " << userInputSide << ", what is the distance for side B or C. \n >";
 		std::cin >> sideB;
 
-		std::cout << "What is the distance for side C. \n >";
-		std::cin >> sideC;
-
-		sideA = sqrt((sideB * sideB) + (sideC * sideC));
-		area = (sideA * sideB) / 2;
+		sideC = sideB;
+		sideA = sideB;
+		area = (sqrt(3) / 4) * (sideA * sideA);
 		perimeter = sideA + sideB + sideC;
 		std::cout << "The distance for side A is " << sideA << "\nThe area of the triangle is " << area << "\nThe perimeter of the triangle is " << perimeter << std::endl;
 	}
